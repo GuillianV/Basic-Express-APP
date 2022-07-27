cf7420734aa4d25ec437eea326bc0df198026f20
lsmnt <leasimonet56@gmail.com>
2022-07-28 00:21:47 +0200

    add db connexion

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var axios = require("axios")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Cadocuir' });
+
+  let jwt = ""
+  axios.post(process.env.DBHOST + 'auth/local', {
+    identifier: "admin",
+    password: "admin123",
+  }).then(response => {
+    jwt = response.data.jwt
+  }).catch(error => {
+    console.log(error)
+  }).finally(()=>{
+    res.render('index', { title: 'Cadocuir', jwt:jwt });
+  });
+
 });
 
 module.exports = router;
