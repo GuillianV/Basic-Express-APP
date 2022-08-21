428c5bc2114d52f42d415453854e63e2846a5d74
lsmnt <leasimonet56@gmail.com>
2022-08-21 12:09:34 +0200

    revome strapi request

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var fs = require('fs')
+var bodyParser = require('body-parser')
 var cookieParser = require('cookie-parser');
+
+//multer permet d'enregitrer des fichiers tels que des photos ou autres documents
+var multer = require('multer');
 var logger = require('morgan');
 var app = express();
 
@@ -10,19 +14,35 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//Log des requettes utilisateur
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//Permet d'obtenir le coockies utilisateur (req.cookies)
 app.use(cookieParser());
+
+//Chemin static d'acces aux fichiers
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Routes
-app.use(`/`, require(`./routes/index`));
+//Acces au body de la requette (req.body)
+app.use(bodyParser.urlencoded({ extended: true }))
+app.enable('trust proxy');
 
 
 
+// Load Routes
+let routesFolder = "./routes"
+app.use(`/`, require(`${routesFolder}/index`));
+fs.readdirSync( path.join(__dirname,"/routes"))?.forEach(file =>{
 
+  if (!file.includes("index")){
+    console.log(`/${file.replace(".js","")}`)
+    console.log(`${routesFolder}/${file.replace(".js","")}`)
 
+    app.use(`/${file.replace(".js","")}`, require(`${routesFolder}/${file.replace(".js","")}`));
+  }
+});
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var axios = require("axios")
+//var axios = require("axios")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  let jwt = ""
-  axios.post(process.env.DBHOST + 'auth/local', {
-    identifier: "admin",
-    password: "admin123",
-  }).then(response => {
-    jwt = response.data.jwt
-  }).catch(error => {
-    console.log(error)
-  }).finally(()=>{
-    res.render('index', { title: 'Cadocuir', jwt:jwt });
-  });
+
+  res.render('index', { title: 'Cadocuir' });
+
 
 });
 
