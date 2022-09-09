0e6ac3714ce82939024705662ccb88ae71356870
lsmnt <leasimonet56@gmail.com>
2022-09-09 21:45:53 +0200

    add secured connexion

--- modules/database/mongoose/mongooseInit.js
+++ modules/database/mongoose/mongooseInit.js
@@ -2,8 +2,8 @@ require("dotenv").config()
 const mongoose = require('mongoose');
 
 //-------------Connexion a la base de données
+const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`;
 
-var uri = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 main().catch(err => console.log(err));
 
 async function main() {
